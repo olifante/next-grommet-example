772a6e1bfb0c7d04c5acc558805ebbbe11e79c3a
Tiago Henriques <trinosauro@gmail.com>
2016-11-11 16:58:57 +0100

    Add example of Grommet with next.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+import Button from 'grommet/components/Button'
+
+export default () => (
+  <div>
+    <Head>
+      <link href="//cdnjs.cloudflare.com/ajax/libs/grommet/1.0.1/grommet.min.css" rel="stylesheet" type="text/css" />
+    </Head>
+    <h2>Demo of Grommet button with next.js</h2>
+    <Button label="Sample Grommet Button" primary={true} onClick={true} />
+  </div>
+)
