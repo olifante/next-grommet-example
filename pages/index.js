29cf3b3bc7f99fd21f93dd293524346d4150749d
Tiago Henriques <trinosauro@gmail.com>
2017-01-11 19:18:57 +0100

    Add dummy onClick handler

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,6 @@ export default () => (
       <link href="//cdnjs.cloudflare.com/ajax/libs/grommet/1.0.1/grommet.min.css" rel="stylesheet" type="text/css" />
     </Head>
     <h2>Demo of Grommet button with next.js</h2>
-    <Button label="Sample Grommet Button" primary={true} onClick={true} />
+    <Button label="Sample Grommet Button" primary={true} onClick={function() {}} />
   </div>
 )
